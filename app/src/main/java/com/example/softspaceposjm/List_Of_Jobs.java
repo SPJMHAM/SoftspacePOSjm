4f6b9ff40d963f90f89a9d9512e23a1077fe4702
Jun Ming <junming.ham@softspace.com.my>
2019-04-10 16:19:12 +0800

    +UI update

--- app/src/main/java/com/example/softspaceposjm/List_Of_Jobs.java
+++ app/src/main/java/com/example/softspaceposjm/List_Of_Jobs.java
@@ -2,6 +2,44 @@ package com.example.softspaceposjm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.bumptech.glide.Glide;
+import com.example.softspaceposjm.Model.JobInfo1;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.softspaceposjm.ViewHolder.MenuViewHolder;
+import com.google.firebase.database.Query;
+import java.util.ArrayList;
+import java.util.Calendar;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class List_Of_Jobs extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/softspaceposjm/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,4 @@
+package com.example.softspaceposjm.ViewHolder;
+
+public class MenuViewHolder {
+}

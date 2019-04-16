9c766643e6693d2cf6896a69c21d62c33d32f0e4
LeeMunKit <munkit.lee@softspace.com.my>
2019-04-16 17:20:36 +0800

    12/4 5:20pm

--- app/src/main/java/com/example/softspaceposjm/Req_Service/status.java
+++ app/src/main/java/com/example/softspaceposjm/Req_Service/status.java
@@ -1,12 +1,16 @@
 package com.example.softspaceposjm.Req_Service;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 import com.example.softspaceposjm.R;
+import com.example.softspaceposjm.services.Services_Activity;
 
 public class status extends AppCompatActivity {
     private TabLayout tabLayout;
@@ -29,5 +33,29 @@ public class status extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if(itemId == R.id.navigation_home){
+                    Intent home = new Intent(status.this, Services_Activity.class);
+                    startActivity(home);
+                }
+
+//                switch (menuItem.getItemId()){
+//                    case R.id.navigation_home:
+//                        Intent home = new Intent(status.this, Services_Activity.class);
+//                        startActivity(home);
+//                        break;
+//                    case R.id.navigation_dashboard:
+//                        Intent dashb = new Intent(status.this, status.class);
+//                        startActivity(dashb);
+//                        break;
+//                }
+                return true;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/softspaceposjm/services/Services_Activity.java
+++ app/src/main/java/com/example/softspaceposjm/services/Services_Activity.java
@@ -1,10 +1,15 @@
 package com.example.softspaceposjm.services;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.softspaceposjm.R;
+import com.example.softspaceposjm.Req_Service.status;
+
 import android.content.Intent;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -19,16 +24,12 @@ public class Services_Activity extends AppCompatActivity {
         landView = findViewById(R.id.LandView);
         marineView = findViewById(R.id.MarineView);
         privateInvest = findViewById(R.id.PrivateView);
+        BottomNavigationView bottomNav = findViewById(R.id.btm_nav);
 
         landView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 startActivity(new Intent(Services_Activity.this, serviceInfo_Activity.class));
-
-
-
             }
         });
 
@@ -43,6 +44,18 @@ public class Services_Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Services_Activity.this, serviceInfo_Activity.class));
+            }
+        });
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.navigation_dashboard) {
+                    Intent home = new Intent(Services_Activity.this, status.class);
+                    startActivity(home);
+                }
+                return true;
             }
         });
     }
